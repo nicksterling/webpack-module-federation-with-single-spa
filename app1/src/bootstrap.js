7af117fa7257675a214b11ff19b68f71a5c8d656
Nick Sterling <nsterling@vmware.com>
2022-11-04 11:15:47 -0600

    Update App 1

--- app1/src/bootstrap.js
+++ app1/src/bootstrap.js
@@ -1,11 +1,8 @@
 import { registerApplication, start } from 'single-spa';
-import * as header from 'header/header';
-// import * as Demo from 'app2/Demo';
-import * as nav from 'nav/nav';
 
 registerApplication({
   name: 'header',
-  app: header,
+  app: () => import('header/header'),
   activeWhen: '/',
   customProps: {
     some: 'value',
@@ -33,7 +30,7 @@ registerApplication({
 // Config with more expressive API
 registerApplication({
   name: 'nav',
-  app: nav,
+  app: () => import('nav/nav'),
   activeWhen: '/',
   customProps: {
     some: 'value',
